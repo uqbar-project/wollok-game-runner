fef7838d1456d5178119f089df3feb9127bf6d08
ivojawer <ivojawerbaum@gmail.com>
2025-07-09 14:50:43 -0300

    fix: duplicate canvas - remove p5 instace on cleanup

--- app/.client/game.client.tsx
+++ app/.client/game.client.tsx
@@ -4,8 +4,11 @@ export function Game(props: {settings: ConstructorParameters<typeof LocalGame>[0
   const canvas = useRef<HTMLDivElement>(null)
   useEffect(() => {
     console.log(canvas.current)
-    if(canvas.current)
-      new LocalGame(props.settings).start(canvas.current)
+    
+    if(canvas.current){
+      let p5Instance = new LocalGame(props.settings).start(canvas.current)
+      return p5Instance.remove.bind(p5Instance)
+    }
   }, [])
 
 
