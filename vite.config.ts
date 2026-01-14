7c1d9aff83198d6391e145dd9d459967343ae0e5
ivojawer <ivojawerbaum@gmail.com>
2026-01-13 22:06:27 -0300

    prepare for vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import commonjs from 'vite-plugin-commonjs';
-
+import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import commonjs from 'vite-plugin-commonjs'
 
 export default defineConfig({
-  plugins: [
-    commonjs(), 
-    tailwindcss(), 
-    reactRouter(), 
-    tsconfigPaths(), 
-  ],
-});
+  base: './',
+  build: {
+    outDir: 'build/client',
+    sourcemap: false,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore sourcemap errors that break Vercel
+        if (
+          warning.message?.includes("Can't resolve original location of error")
+        )
+          return
+        warn(warning)
+      },
+    },
+  },
+  plugins: [commonjs(), tailwindcss(), reactRouter(), tsconfigPaths()],
+})
